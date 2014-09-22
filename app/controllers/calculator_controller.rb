f9d752c8b2769bec702a6145f7071dfebf19d284
nexus20001 <nexus20001@gmail.com>
2014-09-22 15:50:15 +0300

    add views

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -8,11 +8,11 @@ class CalculatorController < ApplicationController
   end
   private
   def check_perms
-    return _run if @user.admin? and @user.vuln?(params[:operation])
-    _run if @user.able?( params[:value1].to_f , params[:value2].to_f , params[:operation])
+    return _run if @user.admin? and @user.able_admin?(params[:expression])
+    _run if @user.able?(params[:expression]) and @user.correct_value?(params[:expression])
   end
 
   def _run
-     eval("%s %s %s" % [ params[:value1].to_f, params[:operation],  params[:value2].to_f]).to_f
+     eval("%s" % [ params[:expression]]).to_f
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	OPERATIONS = '+-*/'
+	OPERATIONS = '+\-*\/'
 	def admin? 
 		email == 'admin@u.u' 
 	end
-	def vuln?(operation)
-		operations.include? OPERATIONS
+	def able_admin?(expression)
+    expression =~ /^[0-9#{OPERATIONS}]+[0-9]$/
 	end
-	def able?(val1, val2, operation)
-		correct_value?(val1, val2) && correct_opperation?(operation)
+  def able?(expression)
+    expression =~ /^[0-9#{operations}]+[0-9]$/
+  end
+
+	def correct_value?(expression)
+    numbers = expression.scan(/(\d+)/).flatten
+    numbers.each { |s| return false if s.to_f > number.to_f}
 	end
 
-	def correct_value?(val1, val2)
-		val1.to_f <  number.to_f && val2.to_f < number.to_f
-	end
-
-	def correct_opperation?(operation)
-		operations.include? operation
-	end
 end
